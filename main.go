a752fb72913e5b1c0a75b634d3937a822188a76c
Shaun <xshaun97@gmail.com>
2025-02-09 02:13:14 +0800

    add env file

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
-
 func main() {
-    fmt.Println("Hello, World!")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+		bot.WithWebhookSecretToken(os.Getenv("EXAMPLE_TELEGRAM_WEBHOOK_SECRET_TOKEN"))
+	}
+
+	b, _ := bot.New(os.Getenv("EXAMPLE_TELEGRAM_BOT_TOKEN"), opts...)
+
+	// call methods.SetWebhook if needed
+
+	go b.StartWebhook(ctx)
+
+	http.ListenAndServe(":2000", b.WebhookHandler())
+
+	// call methods.DeleteWebhook if needed
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
